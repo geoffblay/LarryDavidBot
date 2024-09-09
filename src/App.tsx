3a77814291dc1ae57de84dbf3994b15301e98a0d
Geoff Blaylock <gblaylock2024@gmail.com>
2024-09-08 18:34:58 -0700

    message send console logging

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import Header from './components/header'
 import { Input } from './components/ui/input'
 import { Button } from './components/ui/button'
-// import { ChatBubble } from './components/chat-bubble'
 import { ChatBubble } from './components/chat-bubble'
+import { useState } from 'react'
 
 
 function App() {
+  const [inputValue, setInputValue] = useState('')
+
+  const handleSend = async (event: React.FormEvent) => {
+    event.preventDefault()
+    console.log(inputValue)
+    setInputValue('')
+  }
+
+
   return (
     <div className="flex flex-col h-screen items-center">
       <Header />
@@ -46,10 +55,15 @@ function App() {
           timestamp="12:40 PM"
         />
       </div>
-      <div className="border-4 border-red-500 mx-auto mt-auto pb-12 flex w-full max-w-sm items-center space-x-2">
-        <Input type="text" placeholder="tell me something interesting." />
+      <form onSubmit={handleSend} className="border-4 border-red-500 mx-auto mt-auto pb-12 flex w-full max-w-sm items-center space-x-2">
+        <Input
+          type="text"
+          placeholder="tell me something interesting."
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)}
+        />
         <Button type="submit">send</Button>
-      </div>
+      </form>
     </div>
   )
 }
