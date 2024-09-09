befdaa7f961fc4c64c6458139a9f198641ed2dee
Geoff Blaylock <gblaylock2024@gmail.com>
2024-09-08 17:46:10 -0700

    basic page layout, input at bottom

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
 import Header from './components/header'
+import { Input } from './components/ui/input'
+import { Button } from './components/ui/button'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <div className="flex flex-col h-screen">
       <Header />
-    </>
+      <div className="mx-auto mt-auto mb-12 flex w-full max-w-sm items-center space-x-2">
+        <Input type="text" placeholder="tell me something interesting." />
+        <Button type="submit">Send</Button>
+      </div>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,11 @@ export default {
   ],
   theme: {
   	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
+  		// borderRadius: {
+  		// 	lg: 'var(--radius)',
+  		// 	md: 'calc(var(--radius) - 2px)',
+  		// 	sm: 'calc(var(--radius) - 4px)'
+  		// },
   		colors: {}
   	}
   },
